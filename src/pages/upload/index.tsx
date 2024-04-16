d646c4ee572196a6df70098c9035f1d257c7d99b
DJWL <1602951127@qq.com>
2024-04-16 14:36:59 +0800

    feat: 又拍云联盟

--- src/pages/upload/index.tsx
+++ src/pages/upload/index.tsx
@@ -1,13 +1,31 @@
-import { FC } from "react";
-import FileUpload from "./FileUpload";
-import { Box, Card } from "@chakra-ui/react";
+import { FC } from 'react';
+import FileUpload from './FileUpload';
+import { Box, Card, Link } from '@chakra-ui/react';
+import Youpaiyun from '../../../public/又拍云_logo8.svg';
 
 const UploadPage: FC = () => {
   return (
-    <Box padding="16px 16px">
+    <Box padding="16px 16px" style={{ height: 'calc(100vh - 60px)', position: 'relative' }}>
       <Card padding="16px">
         <FileUpload />
       </Card>
+      <Link
+        color="teal.500"
+        style={{
+          position: 'absolute',
+          bottom: 10,
+          textAlign: 'center',
+          display: 'flex',
+          alignItems: 'center',
+          left: '50%',
+          transform: 'translateX(-50%)',
+        }}
+        href="https://www.upyun.com/?utm_source=lianmeng&utm_medium=referral"
+      >
+        <span>本网站由</span>
+        <img style={{ height: 100, marginTop: 20 }} src="../../../public/又拍云_logo8.svg"></img>
+        <span>提供CDN加速/云储存服务</span>
+      </Link>
     </Box>
   );
 };
